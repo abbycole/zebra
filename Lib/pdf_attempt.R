95f12267b0e98d1e2412cf6c7f4de6ab75ed727e
sheik086 <sheik085@umn.edu>
2017-04-13 14:43:26 -0500

    Fixed r code
    complete code

--- Lib/pdf_attempt.R
+++ Lib/pdf_attempt.R
@@ -1,1 +1,16 @@
 items <- read.csv("MCTs_20239_Items.csv", TRUE, ",")
+test <- items[items$UserName == "MCTs05",]
+Protein <- mean(test$PROT)
+FAT <- mean(test$TFAT)
+Cals <- mean(test$KCAL)
+Carbs <- mean(test$CARB)
+
+## @knitr part1
+Daily_Prot <- by( test$PROT, test$RecordDayNo, mean)
+Daily_Fat <- by( test$PROT, test$RecordDayNo, mean)
+Daily_Cals <- by( test$PROT, test$RecordDayNo, mean)
+Daily_Carbs <- by( test$PROT, test$RecordDayNo, mean)
+
+## @knitr part2
+plot(Daily_Prot,type = "b", col = "red", xlab = "Day", ylab = "Prot Amount",main = "Protein Intake")
+
