9896c1f898a4e44f1a82bb9f47c36a7c154d10a7
abbycole <abigail.j.cole@gmail.com>
2017-04-17 15:32:57 -0500

    Fixed structure
    Updated readme with this weeks goals and fixed the structure of the
    project.

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
+
+
 # zebra
 Project to create pdf output of individualized study data for study participants
 
+Note: need to have LaTex installed prior to running this code
 
 Example from Stack Exchange that might help: 
 http://stackoverflow.com/questions/15396755/using-loops-with-knitr-to-produce-multiple-pdf-reports-need-a-little-help-to
+
+This week, Abdi will make the main script output to the output folder.
+
+And will learn ggplot - Stretch goal to make a stacked barchart that shows total intake by macronutrient (Carb, Pro, Fat) per day
+
+Attainable goal is just get a graph in ggplot to work.
--- bin/pdf_attempt.R
+++ bin/pdf_attempt.R
@@ -1,11 +1,20 @@
-setwd("C:\\Users\\Surface Pro 3\\Documents\\GitHub\\zebra\\raw") # set to zebra directory
+
+
+library(rmarkdown)
+library(knitr)
+library(ggplot2)
+
+setwd("/Users/abby/Documents/Projects/zebra/raw/") # set to zebra directory
 
 map <- read.table("new_map_with_treatment.txt", sep = "\t", header = TRUE, comment = "") #load mapping file
 
-setwd("C:\\Users\\Surface Pro 3\\Documents\\GitHub\\zebra\\lib")
-library("rmarkdown")
-library("knitr")
+setwd("/Users/abby/Documents/Projects/zebra/lib")
+
+
+
 for (id in unique(map$UserName)){
   subgroup <- map[map$UserName == id,]
   render("MCTS_pdf.rmd",output_file = paste0('report.', id, '.pdf'),"pdf_document")    
 }
+
+
