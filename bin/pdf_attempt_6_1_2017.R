c4876a5e1c9d4a46e307803baa00ed735fa35f28
ArzangS <arzang.syed@gmail.com>
2017-06-06 04:06:55 -0500

    Adding Beta Diversity Plot - Arzang
    Added pertinent code to generate PCOA plot depicting dissimilarity between the microbiomes of pertinent subjects within the study. The current format of this PCOA plot is colored on a "per subject" basis and accounts for all timesteps.

--- bin/pdf_attempt_6_1_2017.R
+++ bin/pdf_attempt_6_1_2017.R
@@ -7,6 +7,9 @@ library(reshape2)
 library(tibble)
 library(data.table)
 library(vegan)
+library(ape)
+
+####Pre Processing####
 
 #set to working directory to project zebra directory
 setwd("C:/Users/arzan/Desktop/Bioinformatics/MCT/zebra-master/zebra-master")
@@ -80,12 +83,12 @@ for (id in unique(map$UserName)){
   
  
   
- #render(input = "lib/MCTS_pdf_mcb.rmd",output_file = paste0('report.', id, '.pdf'),"pdf_document",
-         #output_dir = "output/5.31.test3") 
+ render(input = "lib/MCTS_pdf_mcb.rmd",output_file = paste0('report.', id, '.pdf'),"pdf_document",
+         output_dir = "output/5.31.test3") 
 }
 
 
-###alpha diversity###
+#### Alpha Diversity ####
 
 taxa = taxa[,colSums(taxa)>=20000] #use taxa var to create OTU table (counts)
 
@@ -108,6 +111,83 @@ for (id in unique(map$UserName)){
  render(input = "lib/MCTS_pdf_mcb.rmd",output_file = paste0('report.', id, '.pdf'),"pdf_document",
          output_dir = "output/alpha_d_output") 
 }
+
+
+#### Beta Diversity ####
+
+taxa = taxa[,colSums(taxa)>=20000] #use taxa var to create OTU table (counts)
+
+betataxa<- taxa[(colnames(taxa) %in% map[,"X.SampleID"])] #Create table with just taxa and subjects
+
+betataxa <- t(betataxa) #transpose the table to get to OTU format
+
+#calculate beta diversity and assign to variable betad - method is "w" for now
+betad <-betadiver(betataxa, method="w", order = FALSE, help = FALSE)
+
+#betad as matrix 
+betad <- as.matrix(dist(betad))
+
+#column name is "IDs"
+IDs <-colnames(betad)
+
+# Run the pcoa() function on the beta diversity table, and store the vectors generated as a dataframe 
+PCOA <- data.frame(pcoa(betad)$vectors)
+
+#create vector with placeholders
+
+new_names <- rep("",ncol(PCOA))
+
+#create for loop which gives column names (currently named "axis") the name of PC1, PC2, PC3.. so forth
+#Each column for the PCOA matrix is a new PC (number).
+
+for(i in 1:ncol(PCOA)){
+  new_names[i] <- paste("PC",i,sep="")  
+}
+
+#code above uses a for loop - from the range of 1 to the number of columns in the PCOA
+#matrix, the for loop accesses the new name variable and names it PC + i (which is 1,2,3  depending on column
+#as indicated by for loop - for loop increases in increments of 1 (i goes up by 1) and goes thru columns)
+
+#new_names (PC1, PC2, etc..) replace the current colnames
+names(PCOA) <- new_names
+
+#creates column that is sampleID
+PCOA$X.SampleID <- IDs
+PCOA$UserName <- IDs
+
+#merge map and PCOA by SampleID
+PCOA <- merge(PCOA, map, by = "X.SampleID")
+PCOA <- merge(map,PCOA, by = "UserName")
+
+#Plot PCOA
+ggplot(PCOA) +
+  geom_point(aes(x=PC1,y=PC2, color=UserName)) +  #gives axes and item of focus
+  labs(title="PCOA plot")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
