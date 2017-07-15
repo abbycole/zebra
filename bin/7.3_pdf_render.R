63d62e86a69bdd86b63e2758ac6c838f2b73ecc6
ArzangS <arzang.syed@gmail.com>
2017-07-14 21:56:46 -0500

    Incorporation of suggested changes - Arzang
    Addition of most suggestions for improvement provided, currently in the process of troubleshooting a universal color scheme and way to lay out reference columns

--- bin/7.3_pdf_render.R
+++ bin/7.3_pdf_render.R
@@ -53,7 +53,7 @@ for (id in unique(map$UserName)){
   betataxa<- staxa[(colnames(staxa) %in% map[,"X.SampleID"])] #Create table with just taxa and subjects
   betataxa <- t(betataxa) #transpose 
   render(input = "lib/MCTS_pdf_mcb_7_3.Rmd",output_file = paste0('report.', id, '.pdf'),"pdf_document",
-         output_dir = "output/7.3.output") 
+         output_dir = "output/7.14.output") 
 }
 
 
