025f8217b90b63df22b277b0464b5f8f6389f5d6
ArzangS <arzang.syed@gmail.com>
2017-07-20 17:37:17 -0500

    Arzang- New Figure Added (very raw)
    Added a rough version of the new food based figure. There is a lot to fix and correct, but pushing my changes now since I finally got a figure to appear

--- bin/7.18_pdf_render.R
+++ bin/7.18_pdf_render.R
@@ -36,8 +36,17 @@ rownames(food_map) <- food_map$taxonomy
 food_map$taxonomy <- NULL #removing taxa string column
 food_map$X.FOODID <- NULL #removing food names column
 
-#merge mapping file and food map by UserName to aid through iteration
-food_merge <-  merge(x=map, y=food_map, by= "X.SampleID")
+#Bring food taxa strings from row names to first column using setDT. Food_map is now analagous to subtaxasp
+#food_map <- setDT(food_map, keep.rownames = TRUE)
+
+#melt food_map to create a data drame called melted_food to merge by SampleIDs
+melted_food<- melt(food_map)
+
+#merge melted_food and map by variable (SampleID -- called variable in melted food file - so we use by.x)
+merged_food<- merge(x=melted_food, y=map, by.x = "variable", by.y= "X.SampleID", all.x=TRUE)
+
+colnames(merged_food)[3] <- "UserName" #Change col name from UserName.x to UserName - compatibility purposes
+colnames(merged_food)[4] <- "StudyDayNo" #Change col name from StudyDayNo.x to StudyDayNo - compatibility purposes
 
 #### Read in taxa table #### 
 taxa <- read.delim("raw/youbet.txt", row =1)
@@ -69,12 +78,24 @@ colnames(map)[3] <- "StudyDayNo" #Change col name from StudyDayNo.x to StudyDayN
 
 for (id in unique(map$UserName)){
   
+  #microbiome iteration
   submap <- map[map$UserName == id,] #create submap variable we can use to iterate through 
   subtaxa <- staxa[(colnames(staxa) %in% submap[,"X.SampleID"])] #create subtaxa variable - looks at each subject individually
-  subtaxasp <- setDT(subtaxa, keep.rownames=TRUE) 
+  subtaxasp <- setDT(subtaxa, keep.rownames=TRUE) #make rownames first column of frame
+  
+  #food iteration
+  sub_foodmap <- merged_food[merged_food$UserName == id,] #sub food variable allows us to access one subject at a time (like submap)
+  sub_food <- food_map[(colnames(food_map) %in% sub_foodmap[,"variable"])]#sub food variable looks at each subject individually
+  sub_foodsp <- setDT(sub_food, keep.rownames = TRUE) #make rownames first column of frame
+  
+  #alpha diversity iteration
   subtaxaalpha <- taxa[(colnames(taxa) %in% submap[,"X.SampleID"])] #subtaxa variable used for alpha diversity
+ 
+  #beta diversity iteration
   betataxa<- staxa[(colnames(staxa) %in% map[,"X.SampleID"])] #Create table with just taxa and subjects
   betataxa <- t(betataxa) #transpose 
+  
+  #rendering
   render(input = "lib/MCTS_pdf_mcb_7_3.Rmd",output_file = paste0('report.', id, '.pdf'),"pdf_document",
          output_dir = "output/7.20.output") 
 }
